d0215aca45ac0e68c3aa6c1d283130260f000751
AleksandrTomskiy24212 <a.tomskii@g.nsu.ru>
2025-10-17 02:39:36 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include "WordCounter.h"
 #include "CSVWriter.h"
+#include <string>
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
         return 1;
     }
+
+    std::string inputFilename = argv[1];
+    std::string outputFilename = argv[2];
+
     WordCounter counter;
-    counter.processFile(argv[1]);
+    counter.processFile(inputFilename);
 
     CSVWriter writer;
-    writer.write(counter.getSortedWordsPublic(), counter.getTotalWords(), argv[2]);
-    
+    writer.write(counter.getSortedWords(), counter.getTotalWords(), outputFilename);
+
     return 0;
 }
