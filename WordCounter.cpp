aaec004bb853308e2dd1a7f907d2293cf4ded081
AleksandrTomskiy24212 <a.tomskii@g.nsu.ru>
2025-10-17 02:37:45 +0700

    Update WordCounter.cpp

--- WordCounter.cpp
+++ WordCounter.cpp
@@ -8,22 +8,15 @@ bool WordCounter::compareWordFrequency(std::pair<std::string, int> a,
     return a.second > b.second;
 }
 
-void WordCounter::processLine(std::string line) {
-    std::vector<std::string> words = extractor.extractWords(line);
-    for (std::string word : words) {
-        wordCount[word]++;
-        totalWords++;
-    }
-}
-
 void WordCounter::processFile(std::string inputFilename) {
     std::ifstream inputFile(inputFilename);
     std::string line;
     while (std::getline(inputFile, line)) {
-        lines.push_back(line);
-    }
-    for (std::string l : lines) {
-        processLine(l);
+        std::vector<std::string> words = extractor.extractWords(line);
+        for (const std::string& word : words) {
+            wordCount[word]++;
+            totalWords++;
+        }
     }
 }
 
@@ -31,10 +24,6 @@ std::vector<std::pair<std::string, int>> WordCounter::getSortedWords() {
     std::vector<std::pair<std::string, int>> sortedWords(wordCount.begin(), wordCount.end());
     std::sort(sortedWords.begin(), sortedWords.end(), compareWordFrequency);
     return sortedWords;
-}
-
-std::vector<std::pair<std::string, int>> WordCounter::getSortedWordsPublic() {
-    return getSortedWords();
 }
 
 int WordCounter::getTotalWords() const {
